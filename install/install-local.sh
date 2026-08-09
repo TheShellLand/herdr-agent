ba744b3cd167e834b3f2a297a90284e093867f49
naisanzaa <naisanza@gmail.com>
2026-08-10 04:59:19 +0800

    Update install script to ensure local bin directory exists and update .profile

--- install/install-local.sh
+++ install/install-local.sh
@@ -2,5 +2,13 @@
 
 set -xe
 
+BIN=$HOME/.local/bin
+
+mkdir -p $BIN
+
+if ! grep "$BIN" $HOME/.profile ; then
+  echo """export "PATH=$BIN:$PATH"""" >> $HOME/.profile
+fi
+
 curl -fsSL https://herdr.dev/install.sh | sh
 
