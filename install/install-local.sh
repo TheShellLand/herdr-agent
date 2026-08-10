660640407a0c21476496fe2c8f21a4419892a09c
naisanzaa <naisanza@gmail.com>
2026-08-10 17:54:01 +0800

    symllink instead of setting PATH

--- install/install-local.sh
+++ install/install-local.sh
@@ -2,13 +2,22 @@
 
 set -xe
 
-BIN=$HOME/.local/bin
-
-mkdir -p $BIN
-
-if ! grep "$BIN" $HOME/.profile ; then
-  echo """export "PATH=$BIN:$PATH"""" >> $HOME/.profile
-fi
+BIN="$HOME/.local/bin"
+HERDR="$BIN/herdr"
 
 curl -fsSL https://herdr.dev/install.sh | sh
+
+chmod ugo+rx "$HERDR"
+
+if sudo touch /usr/bin/.TOUCH 2>/dev/null ; then 
+  sudo rm /usr/bin/.TOUCH
+
+  sudo ln -s $HERDR /usr/bin/herdr
+fi
+
+if sudo touch /usr/local/bin/.TOUCH 2>/dev/null ; then
+  sudo rm /usr/local/bin/.TOUCH
+
+  sudo ln -s $HERDR /usr/local/bin/herdr
+fi
 
